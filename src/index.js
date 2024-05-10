bb02945d5591ef3c65c8e1b0fa535b54105e7b64
Ricky Winarko <65493905+SpiderPigzz@users.noreply.github.com>
2024-05-11 00:43:08 +0800

    changed to prow

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // Import BrowserRouter
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
